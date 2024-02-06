9d2008fcc706d155626b3929e0c48914e3adbea5
achavez101 <amairanygchavez@gmail.com>
2024-02-05 16:05:46 -0800

    promises

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-
-
-
 // Asynchronous JavaScript
 
 // (check) A function that uses the fetch() API
-// A function that is using promises and interacting with promises
-// Use of .then() method
+// (check) A function that is using promises and interacting with promises
+// (check) Use of .then() method
 // (check) Use of the .json() method
-// Use of Promise.all() and Promise.any()
+// (check) Use of Promise.all() and Promise.any()
 // (check) Use of async and await keywords
 // (check) A function that uses web-workers
+// (check) DOM methods
 
 
 const API_URL = 'https://api.quotable.io/random'
@@ -25,20 +23,21 @@ async function fetchQuote(){
         const result = await fetch(API_URL)
         const quote = await result.json()
         quoteContainer.innerHTML = `
-            <p><h3>Quote</h3>${quote.content}</p>
+            <p>${quote.content}</p>
             <cite><strong>Author:</strong> ${quote.author}</cite>
             <p><strong>Type of Quote:</strong> ${quote.tags[0]}</p>
             <p><strong>Date Added:</strong> ${quote.dateAdded}</p>
         `
-        console.log(quote);
+        localStorage.setItem(quote, JSON.stringify(quote));
+        // console.log(quote);
     } catch (error) {
         console.error('no quote');
     }
 }
 fetchQuote();
 
-
-// web worker
+// web worker 
+// followed by a button to start and stop an action
 var worker;
 
 function startWorker() {
@@ -61,17 +60,58 @@ function endWorker() {
     worker = undefined;
 }
 
-
 // Promise
-
-let promise = new Promise((resolve, reject)=>{
-	resolve("Successful");
+// background color change
+const color = (backgroundChange, resolved) => {
+  return new Promise((resolve, reject) => {
+    if (resolved) {
+        setTimeout(() => resolve(backgroundChange), resolved);
+    } else {
+      // returns resolved promise without any delay
+      resolve(backgroundChange); 
+    }
+  })
+}
+// starts at original color
+color("#afb4ad")
+.then((backgroundChange) => {
+    console.log('Original color', "hex-value: " + backgroundChange);
+    document.body.style.backgroundColor = backgroundChange;
+     //returns a resolved promise after (10 second timeout)
+  return color("#848871", 5000);
 })
-.then(e=>{ 
-	console.log(e)
-	return "Completed"
-	})
-.then(e=>{console.log(e)})
+// executes after 10 seconds
+.then((backgroundChange) => {
+  document.body.style.backgroundColor = backgroundChange;
+})
+
+// Promise for Welcome Title
+// let 2 seconds go by before releasing the title
+setTimeout(function() { welcomeFunction("Welcome!"); }, 2000);
+
+function welcomeFunction(value) {
+  document.getElementById("promise").innerHTML = value;
+}
+
+// Print promise to console
+Promise.all([
+  color,
+  welcomeFunction
+])
+.then(dataArray => {
+  localStorage.setItem("Promise All Data", dataArray);
+  console.log("Promise All Data", dataArray);
+})
+.catch(error => {
+  console.log('Error', error)
+})
+
+// Promise Any
+const promises = [color, welcomeFunction];
+Promise.any(promises).then((values) => console.log(values));
+
+
+
 
 
 // API failed to work
